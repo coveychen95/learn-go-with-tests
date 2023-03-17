7111de54abfd31e8b85fefdf50fb4ae1fdc74ae4
kvchen95 <kvchen95@outlook.com>
2023-03-18 00:32:45 +0800

    feat: 1

--- 04-array-slices/sum_test.go
+++ 04-array-slices/sum_test.go
@@ -3,22 +3,22 @@ package array_slices
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func (t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
-	
+
 		got := SumSlices(numbers)
-	
+
 		want := 15
-	
+
 		if got != want {
 			t.Errorf("got %d but expected %d", got, want)
 		}
 	})
 
-	t.Run("collection of any size", func (t *testing.T) {
-		numbers := []int{1,2,3}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-		got := Sum(numbers)
+		got := SumSlices(numbers)
 
 		want := 6
 
