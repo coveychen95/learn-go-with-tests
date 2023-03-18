c43c6bbcece2c46a08cd3a4de7e2d2fdce54cec9
kvchen95 <kvchen95@outlook.com>
2023-03-18 22:11:21 +0800

    feat: float

---
+++ 05-structs/v1/shapes_test.go
@@ -0,0 +1,12 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("want %.2f but got %.2f", want, got)
+	}
+}
